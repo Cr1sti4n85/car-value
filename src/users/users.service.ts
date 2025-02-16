cc800be2b57fd56a5999cc4f3da1dd15a78830d4
Cristian Perez <cperezlecaros@gmail.com>
2025-02-15 17:12:58 -0700

    Add find, update and remove methods to service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { error } from 'console';
 
 @Injectable()
 export class UsersService {
@@ -14,5 +15,32 @@ export class UsersService {
     const user = this.userRepo.create({ email, password });
 
     return this.userRepo.save(user);
+  }
+
+  findOne(id: number) {
+    return this.userRepo.findOne({ where: { id } });
+  }
+
+  find(email: string) {
+    return this.userRepo.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    Object.assign(user, attrs);
+    return this.userRepo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    return this.userRepo.remove(user);
   }
 }
