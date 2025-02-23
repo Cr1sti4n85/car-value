dc12561ebc379b4029349533c8f35af05126a17a
Cristian Perez <cperezlecaros@gmail.com>
2025-02-23 14:11:29 -0700

    Create auth.e2e and refactor app.module and main

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+// import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import cookieSession = require('cookie-session');
+// const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['pass909090'],
-    }),
-  ),
-    app.useGlobalPipes(
-      new ValidationPipe({
-        whitelist: true,
-      }),
-    );
+  // app.use(
+  //   cookieSession({
+  //     keys: ['pass909090'],
+  //   }),
+  // ),
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     whitelist: true,
+  //   }),
+  // );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,30 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { App } from 'supertest/types';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication system', () => {
+  let app: INestApplication<App>;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles signup request', () => {
+    const email: string = 'test1@email.com';
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'password' })
+      .expect(201)
+      .then((response) => {
+        expect(response.body.id).toBeDefined();
+        expect(response.body.email).toEqual(email);
+      });
+  });
+});
