3a0688618294a0214b875093bad54091d95ce8eb
Cristian Perez <cperezlecaros@gmail.com>
2025-02-23 17:46:13 -0700

    Update auth.e2e-spec

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -16,15 +16,33 @@ describe('Authentication system', () => {
     await app.init();
   });
 
-  it('handles signup request', () => {
+  it('handles signup request', async () => {
     const email: string = 'test1@email.com';
-    return request(app.getHttpServer())
+    const response = await request(app.getHttpServer())
       .post('/auth/signup')
       .send({ email, password: 'password' })
-      .expect(201)
-      .then((response) => {
-        expect(response.body.id).toBeDefined();
-        expect(response.body.email).toEqual(email);
-      });
+      .expect(201);
+
+    expect(response.body.id).toBeDefined();
+    expect(response.body.email).toEqual(email);
+  });
+  it('sign up and get currently logged in user', async () => {
+    const email: string = 'test2@email.com';
+    const response = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'password' })
+      .expect(201);
+
+    const cookie = response.get('Set-Cookie');
+
+    if (!cookie) {
+      throw new Error('Cookie not set');
+    }
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+    expect(body.email).toEqual(email);
   });
 });
