55a95a8ea5f0ca013c0644638e4d33189c562105
Cristian Perez <cperezlecaros@gmail.com>
2025-02-25 10:55:31 -0700

    Update reports controller and service

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -3,14 +3,16 @@ import { CreateReportDto } from './dto/create-report.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Report } from './report.entity';
+import { User } from '../users/user.entity';
 
 @Injectable()
 export class ReportsService {
   constructor(
     @InjectRepository(Report) private reportRepo: Repository<Report>,
   ) {}
-  create(body: CreateReportDto) {
-    const newCar = this.reportRepo.create(body);
-    return this.reportRepo.save(newCar);
+  create(body: CreateReportDto, user: User) {
+    const report = this.reportRepo.create(body);
+    report.user = user;
+    return this.reportRepo.save(report);
   }
 }
