be3e230e95c94866b4e773883cf1c3ef4442e93f
Cristian Perez <cperezlecaros@gmail.com>
2025-02-26 16:21:11 -0700

    Create curret-user middleware and update users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
+// import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { UsersController } from './users.controller';
 import { User } from './user.entity';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+// import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { CurrentUserMiddleware } from './middleware/current-user.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])], //creates repository
@@ -13,11 +14,16 @@ import { CurrentUserInterceptor } from './interceptors/current-user.interceptor'
     UsersService,
     AuthService,
     //globally scoped interceptor
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CurrentUserInterceptor,
-    },
+    // {
+    //   provide: APP_INTERCEPTOR,
+    //   useClass: CurrentUserInterceptor,
+    // },
   ],
   controllers: [UsersController],
 })
-export class UsersModule {}
+export class UsersModule {
+  //aplicando middleware de autorizacion
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CurrentUserMiddleware).forRoutes('*');
+  }
+}
